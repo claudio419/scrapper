7cc10d74d39b8e3f06a0955aaee3b0ecf32c16d0
Claudio Roman <claudio.roman@check24.de>
2022-07-06 16:46:11 +0200

    [SCRAPPER-0004] Dynamict Data Drive for params and date added

--- scrapper/soccerstats.scrapper.js
+++ scrapper/soccerstats.scrapper.js
@@ -1,31 +1,43 @@
 const fs = require('fs');
+let nextThreePages = new DataTable(['param', 'date'])
+
+/*
+* dayDate = today.setDate(new Date().getDate()+matchdayParams);
+  dayDate = today.getFullYear()+'_'+(today.getMonth()+1)+'_'+today.getDate();
+* */
+const today = new Date();
+const dayDateToday = today.getFullYear()+'_'+(today.getMonth()+1)+'_'+today.getDate();
+today.setDate(new Date().getDate()+1); // Tomorrow
+const dayDateTomorrow = today.getFullYear()+'_'+(today.getMonth()+1)+'_'+today.getDate();
+today.setDate(new Date().getDate()+2); // After tomorrow
+const dayDateafterTomorrow =  today.getFullYear()+'_'+(today.getMonth()+1)+'_'+ today.getDate();
+
+nextThreePages.add(['1', dayDateToday]);
+nextThreePages.add(['2', dayDateTomorrow]);
+nextThreePages.add(['3', dayDateafterTomorrow]);
 
 Feature('SoccerStats Data extraction').tag('@scrapper');
 
+Data(nextThreePages).Scenario('Extract Data from SoccerStats', async ({ I, current, soccerStatsPage, clientPage}) => {
 
-Scenario('Extract visible Data from SoccerStats', async ({ I, soccerStatsPage, clientPage}) => {
-        const today = new Date()
-        let dayDate = today.getFullYear()+'_'+(today.getMonth()+1)+'_'+today.getDate();
         soccerStatsPage.goToDailyMachtPageWithTermAndConditions(0);
 
         for (let matchdayParams = 1; matchdayParams<4; matchdayParams++)  {
 
-
-                if (fs.existsSync('./output/scrapperSoccerStats_' + dayDate +'.json')) {
-                        dayDate = today.setDate(new Date().getDate()+matchdayParams);
-                        dayDate = today.getFullYear()+'_'+(today.getMonth()+1)+'_'+today.getDate();
-                        continue;
+                if (fs.existsSync('./output/scrapperSoccerStats_' + current.date +'.json')) {
+                        return;
                 }
 
                 let docs = [];
 
-                I.say(`I go to page number ${matchdayParams}`);
+                I.say(`I go to page number ${current.param}`);
 
-                soccerStatsPage.goToDailyMachtPage(matchdayParams);
+                soccerStatsPage.goToDailyMachtPage(current.param);
 
                 let playingLeagues = await soccerStatsPage.getPlayingLeagues(soccerStatsPage.locators.matchsTrsVisible);
                 let hiddenMatchs = await soccerStatsPage.getPlayingLeagues(soccerStatsPage.locators.matchsTrsHidden);
                 playingLeagues.push(... hiddenMatchs);
+                hiddenMatchs = '';
 
                 I.say(`PlayinLeagues are ${playingLeagues.length}`);
 
@@ -39,14 +51,16 @@ Scenario('Extract visible Data from SoccerStats', async ({ I, soccerStatsPage, c
                 });
 
                 const jsonContent = JSON.stringify(docs);
+                matchesData = '';
                 try {
-                         fs.writeFileSync('./output/scrapperSoccerStats_' + dayDate +'.json', jsonContent, 'utf8');
-                        dayDate = today.setDate(new Date().getDate()+matchdayParams);
-                        dayDate = today.getFullYear()+'_'+(today.getMonth()+1)+'_'+today.getDate();
+                         fs.writeFileSync('./output/scrapperSoccerStats_' + current.date +'.json', jsonContent, 'utf8');
+
                 } catch (err) {
                         console.error(err);
                 }
 
+                docs = [];
+                playingLeagues = '';
         }
 
 });
